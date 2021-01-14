9673d472ebea5eb28725baeb39a8cdb548f8b807
LEESEONGBOK1214 <66585769+LEESEONGBOK1214@users.noreply.github.com>
2021-01-15 01:42:10 +0900

    create Jumin SpringData

---
+++ 캡스톤/JuminProject/src/test/java/com/yjcapston/juminjroject/JuminjrojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjcapston.juminjroject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuminjrojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
