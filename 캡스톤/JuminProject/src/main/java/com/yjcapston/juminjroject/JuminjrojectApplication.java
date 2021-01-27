0ca4c74e302751c7416637628ce5f982153aacea
LEESEONGBOK1214 <66585769+LEESEONGBOK1214@users.noreply.github.com>
2021-01-27 22:23:35 +0900

    update 2021-01-27

---
+++ 캡스톤/JuminProject/src/main/java/com/yjcapston/juminjroject/JuminjrojectApplication.java
@@ -0,0 +1,13 @@
+package com.yjcapston.juminjroject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuminjrojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuminjrojectApplication.class, args);
+    }
+
+}
