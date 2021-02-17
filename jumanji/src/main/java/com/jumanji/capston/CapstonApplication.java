0dda15727ba7f1c5f6105745b666833ca715e23b
dlals711 <68318350+dlals711@users.noreply.github.com>
2021-02-17 16:33:49 +0900

    jumanji

---
+++ jumanji/src/main/java/com/jumanji/capston/CapstonApplication.java
@@ -0,0 +1,13 @@
+package com.jumanji.capston;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapstonApplication.class, args);
+    }
+
+}
