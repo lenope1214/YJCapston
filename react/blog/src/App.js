ba1fcf98f6b197995ce04ca2e0c522f06261a760
jsh1029 <70466971+jsh1029@users.noreply.github.com>
2021-02-22 16:41:46 +0900

    2021.02.22_1642

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -5,6 +5,9 @@ import adimg2 from './adimg2.jpg';
 import topimg from './topimg.jpg';
 import juminicon from './주민아이콘.png';
 import downimg from './다운로드.png';
+import peopleimg from './주민사람.png';
+import backimg from './backimg.jpg';
+import shopimg from './매장아이콘.png';
 
 function App() {
   return (
@@ -22,7 +25,7 @@ function App() {
         <div className="topimg"><img 
         src={topimg}
         width='100%'
-        height='800px'
+        height='700px'
         />
         </div>
         <div className="toptext">
@@ -37,7 +40,7 @@ function App() {
           <div className="but-item1-text">
           <p>주문의민족</p>
           <p>모든 기능</p>
-          <p>이용하기~!</p>
+          <p>이용하기!</p>
           </div>
           <div className="but-item1-icon">
           <img src={juminicon} 
@@ -59,8 +62,62 @@ function App() {
             <p>편리하게 사용 가능!</p>
           </div>
         </div>
-        <div className="but-item2"><p>사업자 페이지</p></div>
-        <div className="but-item3"><p>예약하러 가기</p></div>
+        <div className="but-item2">
+          <div className="backimg">
+          <img 
+          src={backimg}
+          width='100%'
+          height='700px'
+          />
+          </div>
+        <div className="but-item2-text">
+          <p>웹으로도</p>
+          <p>가능한</p>
+          <p>사업자 등록</p>
+          <p>매장 관리!</p>
+          </div>
+          <div className="but-item2-icon">
+          <img src={peopleimg} 
+          width='300px'
+          height='300px'
+          />
+          <div className="but-item2-but">
+          <button style={{fontSize: '30px',fontFamily: 'Wemakeprice-Bold',height: '100px',width:'210px'
+          ,borderRadius: '60px',border: 0}}>사업자<br></br>
+          페이지로<br></br>
+          →
+          </button>
+          </div>
+          </div>
+          <div className="but-item2-text2">
+            <p>주민 앱 다운시 모든 기능</p>
+            <p>편리하게 사용 가능!</p>
+          </div>
+        </div>
+        <div className="but-item3">
+        <div className="but-item3-text">
+          <p>집에서</p>
+          <p>매장 둘러보고</p>
+          <p>예약하자!</p>
+          </div>
+          <div className="but-item3-icon">
+          <img src={shopimg} 
+          width='250px'
+          height='250px'
+          />
+          <div className="but-item3-but">
+          <button style={{fontSize: '30px',fontFamily: 'Wemakeprice-Bold',height: '130px',width:'230px'
+          ,borderRadius: '60px',border: 0}}>매장 둘러보고<br></br>
+          예약하기<br></br>
+          →
+          </button>
+          </div>
+          </div>
+          <div className="but-item3-text2">
+            <p>웹에서도 매장 둘러보고</p>
+            <p>예약 가능한 주문의 민족</p>
+          </div>
+        </div>
     </div>
   );
 }
