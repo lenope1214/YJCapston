d33330bec6696dff985712139e4de7e4319f2c32
bunzzeok <68317529+bunzzeok@users.noreply.github.com>
2021-02-26 23:54:05 +0900

    회원가입 창과 로그인창 미완성본

---
+++ src/components/auth/AuthTemplate.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+import palette from '../../lib/styles/palette';
+import {Link} from 'react-router-dom';
+
+const AuthTemplateBlock = styled.div `
+position: absolute;
+left:0;
+top:0;
+bottom:0;
+right:0;
+background:${palette.gray[2]};
+display:flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+`;
+
+const WhiteBox = styled.div`
+.logo-area {
+    display: block;
+    padding-bottom: 2rem;
+    text-align: center;
+    font-weight: bold;
+    font-size: 25px;
+    letter-spacing: 3px;
+}
+box-shadow: 0 0 8px rgba(0,0,0,0.025);
+padding: 2rem;
+width : 400px;
+background: white;
+border-radius: 3px;
+`;
+
+const AuthTemplate = ({ children}) => {
+    return (
+        <AuthTemplateBlock>
+            <WhiteBox>
+                <div className="logo-area">
+                    <Link to ="/">주문의 민족</Link>
+                </div>
+                {children}
+            </WhiteBox>
+        </AuthTemplateBlock>
+    );
+};
+
+export default AuthTemplate;
