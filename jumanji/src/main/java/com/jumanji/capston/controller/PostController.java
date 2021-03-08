a373254b35613779996e180310b1a1bec987d47c
이성복 <dltjdqhr1000@gmail.com>
2021-03-08 12:58:23 +0900

    temp

--- jumanji/src/main/java/com/jumanji/capston/controller/PostController.java
+++ jumanji/src/main/java/com/jumanji/capston/controller/PostController.java
@@ -1,20 +1,30 @@
 package com.jumanji.capston.controller;
 
 import com.jumanji.capston.data.Member;
-import org.springframework.stereotype.Controller;
+import com.jumanji.capston.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/rest")
 public class PostController {
 //    @Autowired
 //    AccountRepository accountRepository;
+    @Autowired
+    MemberRepository memberRepository;
 
     @PostMapping("/join")
-    public Member index(Member m){
-        System.out.println(m);
-        return m;
+    public Member join(@RequestBody Member m){
+        System.out.println("JOIN - member : " + m.toString());
+        Member member = new Member();
+        member.setId(m.getId());
+        System.out.println(member.getId());
+        memberRepository.save(m);
+
+        return member;
     }
 
 
