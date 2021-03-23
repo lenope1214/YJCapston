852d720cc7a0eff67af48d9ad3141f0b1000ec3f
LEESEONGBOK1214 <66585769+LEESEONGBOK1214@users.noreply.github.com>
2021-03-23 23:23:52 +0900

    update navLocalSearch

--- jumanji/src/main/java/com/jumanji/capston/controller/NavSearchController.java
+++ jumanji/src/main/java/com/jumanji/capston/controller/NavSearchController.java
@@ -7,20 +7,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController()
+@RequestMapping("/api/v1")
 public class NavSearchController {
 
     @Autowired
     NaverPlaceSearchService naverPlaceSearchService;
 
-    @GetMapping("/addressApiTest/{keyword}")
+    @GetMapping("/addressSearch/{keyword}")
     public ResponseEntity<?> callAddressApi(@PathVariable String keyword) {
 //        System.out.println("Keyword : " + keyword);
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.add("Content-Type", "text/html; charset=UTF-8");
-
+        System.out.println("키워드 : " + keyword);
         return new ResponseEntity<>(naverPlaceSearchService.searchPlace(keyword), responseHeaders, HttpStatus.OK);
     }
 
