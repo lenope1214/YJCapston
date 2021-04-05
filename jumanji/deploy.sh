3ae7f9d985081c4b9d706149f169d5ba313517ce
LEESEONGBOK1214 <66585769+LEESEONGBOK1214@users.noreply.github.com>
2021-04-06 04:59:20 +0900

    Update deploy.sh

--- jumanji/deploy.sh
+++ jumanji/deploy.sh
@@ -1,22 +1,39 @@
-#!/bin/sh
+#!/bin/bash
 
 REPOSITORY=c:\build
-cd $REPOSITORY
 
-APP_NAME=action_codedeploy
-JAR_NAME=$(ls $REPOSITORY | grep '.jar' | tail -n 1)
-JAR_PATH=$REPOSITORY\$JAR_NAME
+cd $REPOSITORY/springboot-service/
 
-CURRENT_PID=$(pgrep -f $APP_NAME)
+echo "> Git Pull"
 
-if [ -z $CURRENT_PID ]
-then
-  echo "> 종료할것 없음."
+git pull
+
+echo "> 프로젝트 Build 시작"
+
+./gradlew build
+
+echo "> Build 파일 복사"
+
+cp ./build/libs/*.jar $REPOSITORY/
+
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+
+CURRENT_PID=$(pgrep -f springboot-webservice)
+
+echo "$CURRENT_PID"
+
+if [ -z $CURRENT_PID ]; then
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -9 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
+    echo "> kill -2 $CURRENT_PID"
+    kill -9 $CURRENT_PID
+    sleep 5
 fi
 
-echo "> $JAR_PATH 배포"
-nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+echo "> 새 어플리케이션 배포"
+
+JAR_NAME=$(ls $REPOSITORY/ |grep 'springboot-webservice' | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+nohup java -jar $REPOSITORY/$JAR_NAME &
