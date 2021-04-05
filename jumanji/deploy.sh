efbc0373dcd3331cbebef84df81510f46b89a9f2
LEESEONGBOK1214 <66585769+LEESEONGBOK1214@users.noreply.github.com>
2021-04-06 03:27:54 +0900

    update scripts

--- jumanji/deploy.sh
+++ jumanji/deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-REPOSITORY=/home/ubuntu/codedeploy/jumanji/
+REPOSITORY=c:\build
 cd $REPOSITORY
 
 APP_NAME=action_codedeploy
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
-JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+JAR_NAME=$(ls $REPOSITORY | grep '.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY\$JAR_NAME
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
