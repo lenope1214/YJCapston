28fc1c10a37ec99fd1f4eb57a82d0ef7711042c7
이성복 <dltjdqhr1000@gmail.com>
2021-04-06 15:07:08 +0900

    update bat

---
+++ jumanji/deletejar.bat
@@ -0,0 +1,1 @@
+forfiles /P C:\jumanji-service /M capston-0.0.1-SNAPSHOT.jar "CMD /C del @file"
--- jumanji/deploy.bat
+++ jumanji/deploy.bat
@@ -1,7 +1,5 @@
-@ECHO OFF
-SET killport=8088
-for /f "tokens=5" %%p in ('netstat -aon ^| find /i "listening" ^| find "%killport%"') do taskkill /F /PID %%p
+DEL C:\jumanji-service\capston-0.0.1-SNAPSHOT.jar
 
-java -jar C:/Users/Administrator/Documents/GitHub/YJCapston\capston-0.0.1-SNAPSHOT.jar
+java -jar C:\jumanji-service\capston-0.0.1-SNAPSHOT.jar
 
 pause
