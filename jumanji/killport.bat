8e15a99ecd36d58515e38e5b8c914c71f572892d
이성복 <dltjdqhr1000@gmail.com>
2021-04-06 13:40:47 +0900

    Update killport.bat

--- jumanji/killport.bat
+++ jumanji/killport.bat
@@ -1,3 +1,3 @@
 @echo off
-for /f "tokens=5" %%A in ('netstat -ab ^| find /i 8088') do (set PID=%%A)
+for /f "tokens=5" %%A in ('netstat -ab ^| find /i "8088"') do (set PID=%%A)
 taskkill /f /fi "PID eq %PID%"
