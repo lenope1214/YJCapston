e3cda3e8853bcdeede3c936b30adcdd29975aec0
이성복 <dltjdqhr1000@gmail.com>
2021-04-06 10:22:54 +0900

    ㅕ

--- jumanji/deploy.bat
+++ jumanji/deploy.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
-SET killport=8080
+SET killport=8088
 for /f "tokens=5" %%p in ('netstat -aon ^| find /i "listening" ^| find "%killport%"') do taskkill /F /PID %%p
 
-java -jar C:/Users/Administrator/Documents/GitHub/YJCapston\capston-0.0.1-SNAPSHOT.jar
+java -jar C:\jumanji-service\capston-0.0.1-SNAPSHOT.jar
 
 pause
