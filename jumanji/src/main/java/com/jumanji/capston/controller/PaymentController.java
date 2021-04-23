2a766d347bed468775abdb1d7a6a4775b5e03864
LEESEONGBOK1214 <dltjdqhr1000@gmail.com>
2021-04-23 19:22:27 +0900

    테이블 변경으로 인한 변경사항 적용중

--- jumanji/src/main/java/com/jumanji/capston/controller/PaymentController.java
+++ jumanji/src/main/java/com/jumanji/capston/controller/PaymentController.java
@@ -1,36 +1,37 @@
-package com.jumanji.capston.controller;
-
-import com.jumanji.capston.data.Payment;
-import com.jumanji.capston.service.PaymentServiceImpl;
-import lombok.Getter;
-import oracle.jdbc.proxy.annotation.Post;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-//@CrossOrigin(origins = "http://localhost:3000")
-@RequestMapping("/api/v1")
-public class PaymentController {
-
-    @Autowired
-    private PaymentServiceImpl paymentService;
-
-    @Transactional
-    @GetMapping("/payment/{payId}")
-    public ResponseEntity<?> getPayment(@RequestHeader String authorization, @PathVariable String payId){
-        Payment payment = paymentService.get(authorization, payId);
-        Payment.Response response = new Payment.Response(payment);
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
-
-    @Transactional
-    @PostMapping("/payment")
-    public ResponseEntity<?> postPayment(@RequestHeader String authorization, @RequestBody Payment.Request request) {
-        // response 형태로 바꿔줘야함.
-        return new ResponseEntity(paymentService.post(authorization, request), HttpStatus.CREATED);
-    }
-
-}
+//package com.jumanji.capston.controller;
+//
+//import com.jumanji.capston.data.Payment;
+//import com.jumanji.capston.service.OrderServiceImpl;
+//import com.jumanji.capston.service.PaymentServiceImpl;
+//import lombok.Getter;
+//import oracle.jdbc.proxy.annotation.Post;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.transaction.annotation.Transactional;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+////@CrossOrigin(origins = "http://localhost:3000")
+//@RequestMapping("/api/v1")
+//public class PaymentController {
+//
+//    @Autowired
+//    private OrderServiceImpl orderService;
+//
+//    @Transactional
+//    @GetMapping("/payment/{orderId}")
+//    public ResponseEntity<?> getPayment(@RequestHeader String authorization, @PathVariable String payId){
+//        Payment payment = orderService.get(authorization, payId);
+//        Payment.Response response = new Payment.Response(payment);
+//        return new ResponseEntity<>(response, HttpStatus.OK);
+//    }
+//
+//    @Transactional
+//    @PostMapping("/payment")
+//    public ResponseEntity<?> postPayment(@RequestHeader String authorization, @RequestBody Payment.Request request) {
+//        // response 형태로 바꿔줘야함.
+//        return new ResponseEntity(orderService.post(authorization, request), HttpStatus.CREATED);
+//    }
+//
+//}
