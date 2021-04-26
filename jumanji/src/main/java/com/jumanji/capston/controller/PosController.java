fa294e17d71c6c38d62a94dd108b7b8c50c71497
LEESEONGBOK1214 <dltjdqhr1000@gmail.com>
2021-04-26 20:44:45 +0900

    포스기, 환불 api 마저 해야함.

---
+++ jumanji/src/main/java/com/jumanji/capston/controller/PosController.java
@@ -0,0 +1,46 @@
+package com.jumanji.capston.controller;
+
+import com.jumanji.capston.data.OrderMenu;
+import com.jumanji.capston.data.Pos;
+import com.jumanji.capston.data.Shop;
+import com.jumanji.capston.data.Tab;
+import com.jumanji.capston.service.OrderMenuServiceImpl;
+import com.jumanji.capston.service.ShopServiceImpl;
+import com.jumanji.capston.service.TableServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class PosController {
+    @Autowired
+    ShopServiceImpl shopService;
+    @Autowired
+    OrderMenuServiceImpl orderMenuService;
+    @Autowired
+    TableServiceImpl tableService;
+
+
+    @Transactional(readOnly = true)
+    @GetMapping("/shop/{shopId}/pos")
+    public ResponseEntity<?> getShopPos(@PathVariable String shopId){
+        Pos response;
+        Shop shop;
+        List<OrderMenu> orderMenuList;
+        List<Tab> tabList;
+
+        shop = shopService.get(shopId);
+        orderMenuList = orderMenuService.
+
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
--- jumanji/src/main/java/com/jumanji/capston/controller/ShopController.java
+++ jumanji/src/main/java/com/jumanji/capston/controller/ShopController.java
@@ -5,6 +5,7 @@ import com.jumanji.capston.data.Shop;
 import com.jumanji.capston.service.ShopServiceImpl;
 import com.jumanji.capston.service.StorageServiceImpl;
 import com.jumanji.capston.service.UserServiceImpl;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -62,15 +63,7 @@ public class ShopController {
         System.out.println("샵리스트 >> ");
 //        return shopService.getShopList();
         return shopService.getList(category, sortTarget); // 얘는 이렇게 하는게 좋을듯..
-
     }
-
-//    @Transactional(readOnly = true)
-//    @GetMapping("/shopList/{category}") // get /shopList/{category}
-//    public ResponseEntity<?> getShopListByCategory(@PathVariable String category) {
-//        return shopService.getShopListByCat(category);
-////        return new ResponseEntity<>(shopCatList, httpHeaders, HttpStatus.OK); // 이렇게 하면 오류. 객체를 utf로 변환 시켜서 그런지 무슨 한글 변환하면서 오류나나봄!
-//    }
 
     @Transactional
     @PostMapping("/shop") // post /shop 매장등록     Form-data로 받음 => Param. requestbody를 안적으면 자동 param 매핑 해주는듯
