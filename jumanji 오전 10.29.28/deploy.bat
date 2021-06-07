d9b3a4915717bc50cb80edbb4cfeef931b415010
LEESEONGBOK1214 <66585769+LEESEONGBOK1214@users.noreply.github.com>
2021-06-07 11:13:01 +0900

    ..

--- jumanji 오전 10.29.28/deploy.bat
+++
@@ -1,3 +0,0 @@
-java -jar C:\jumanji-service\capston-0.0.1-SNAPSHOT.jar
-
-pause
--- jumanji 오전 10.29.28/src/main/resources/static/js/script.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    // alert('load script.js'); //src="/js/script.js" 하면됨.
-
-});
-
-function testFunc(){
-    alert('testFunc!');
-}
