e886b9f938ab680cfe550af2d88f7c2ad57f3fb6
LEESEONGBOK1214 <dltjdqhr1000@gmail.com>
2021-06-07 10:29:32 +0900

    .

--- jumanji/killport.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-SET killport=8088
-for /f "tokens=5" %%p in ('netstat -aon ^| find /i "listening" ^| find "%killport%"') do taskkill /F /PID %%p
-
-pause
--- jumanji/src/main/java/com/jumanji/capston/repository/CouponRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jumanji.capston.repository;
-
-import com.jumanji.capston.data.Coupon;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CouponRepository extends JpaRepository<Coupon, Long> {
-
-
-}
