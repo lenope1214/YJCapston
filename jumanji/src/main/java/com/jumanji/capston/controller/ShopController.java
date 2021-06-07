e19670e40d9b4fc4dd722400ed2b47e1876ef979
LEESEONGBOK1214 <dltjdqhr1000@gmail.com>
2021-06-07 09:59:05 +0900

    rㅜㅅ

--- jumanji/src/main/java/com/jumanji/capston/controller/ShopController.java
+++ jumanji/src/main/java/com/jumanji/capston/controller/ShopController.java
@@ -36,10 +36,11 @@ public class ShopController {
     StorageServiceImpl storageService;
 
 
+
     @Transactional(readOnly = true) // get /shop/{shopId}
     @GetMapping("shops/{shopId}")  // get shop/{shopId} 식당번호로 식당 조회
     public ResponseEntity<?> getShopById(
-            @Nullable @RequestHeader String authorization,
+            @Nullable @RequestHeader String authorization, // 찜 정보 가져올때 사용.
             @PathVariable String shopId) {
         return shopService.getShopByShopId(authorization, shopId);
     }
@@ -55,7 +56,7 @@ public class ShopController {
     public ResponseEntity<?> getMyShop(@RequestHeader String authorization) { // 수정해야함.
         List<Shop> shopList = shopService.getMyShop(authorization);
         List<Shop.Response> response = new ArrayList<>();
-        for(Shop shop: shopList){
+        for (Shop shop : shopList) {
             response.add(new Shop.Response(shop));
         }
         return new ResponseEntity<>(response, HttpStatus.OK);
@@ -115,6 +116,5 @@ public class ShopController {
 //    private List<Shop> getMyShopList(String loginId) {
 //        return shopService.findByOwnerId(loginId);
 //    }
-
 
 }
