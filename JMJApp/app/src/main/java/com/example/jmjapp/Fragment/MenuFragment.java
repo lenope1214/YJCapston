6f042edd6f56f289875ee75c21b3fff1bf94d22a
LEESEONGBOK1214 <dltjdqhr1000@gmail.com>
2021-06-07 09:03:42 +0900

    ㅇㅇ

--- JMJApp/app/src/main/java/com/example/jmjapp/Fragment/MenuFragment.java
+++ JMJApp/app/src/main/java/com/example/jmjapp/Fragment/MenuFragment.java
@@ -3,6 +3,7 @@ package com.example.jmjapp.Fragment;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
