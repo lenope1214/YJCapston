7336ca778f9036d90567abe618833197aadd46cf
jsh1029 <wpwkfl1029@naver.com>
2021-06-10 16:06:22 +0900

    0610_Shopsocketlist_design
    _

--- src/components/Shopsocketlist/Shopsocketlist.js
+++ src/components/Shopsocketlist/Shopsocketlist.js
@@ -1,34 +1,42 @@
 import React from "react";
 import { requirelist } from "../../lib/MyPage";
+import * as S from "./style";
 
 const Shopsocketlist = ({ list }) => {
     return (
+        <>
+        <S.SocketListWrap>
         <body>
             <div class="jmalllist">
-                <p>주문 현황 목록</p>
-
-                <div>
+                <div className="titlebox">
+                <p className="title">예약 현황 목록</p>
+                </div>
+                <div className="card-box">
                     {list.map((list) => {
                         console.log(list.length);
                         return (
-                            <div>
-                                <div>
-                                    <span>주문자 명 : </span>
+                            <div className="card">
+                                <p className="subtitle">예약 요청</p>
+                                <hr width='95%' className="hr"></hr>
+                                <div className="card-text">
+                                <div className="item">
+                                    <span className="label">● 아이디 &nbsp;&nbsp; : &nbsp;</span>
                                     {list.orderid}
                                 </div>
-                                <div>
-                                    <span>주문 번호 : </span>
+                                <div className="item">
+                                    <span className="label">● 주문번호 : &nbsp;</span>
                                     {list.ordernumber}
                                 </div>
-                                <div>
-                                    <span>주문자 연락처 : </span>
+                                <div className="item">
+                                    <span className="label">● 연락처 &nbsp;&nbsp; : &nbsp;</span>
                                     {list.orderphone}
                                 </div>
-                                <div>
-                                    <span>주문 목록 : </span>
+                                <div className="item">
+                                    <span className="label">● 예약메뉴 : &nbsp;</span>
                                     {list.orderlist}
                                 </div>
-                                <button>수락</button>
+                                </div>
+                                <button className="but1">수락</button>
                                 <button
                                     onClick={async () => {
                                         const res = await requirelist(
@@ -39,6 +47,7 @@ const Shopsocketlist = ({ list }) => {
                                             console.log(res.data);
                                         });
                                     }}
+                                    className="but2"
                                 >
                                     거절
                                 </button>
@@ -48,6 +57,8 @@ const Shopsocketlist = ({ list }) => {
                 </div>
             </div>
         </body>
+        </S.SocketListWrap>
+        </>
     );
 };
 
